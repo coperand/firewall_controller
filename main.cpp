d927a12f45744630a4b0effaef24a3366b8cc7c9
coperand <andrew14.08.1998@yandex.ru>
2021-10-26 21:37:08 +0300

    Selfcontrolling functional

--- main.cpp
+++ main.cpp
@@ -1,5 +1,7 @@
 #include <stdio.h>
 #include <unistd.h>
+#include <signal.h>
+#include <sys/wait.h>
 
 #include "iptc.h"
 #include "snmp_handler.h"
@@ -7,16 +9,48 @@
 
 using namespace std;
 
+bool work = true;
+
+void signal_handler(int sig)
+{
+    work = false;
+}
+
+pid_t start_program()
+{
+    pid_t pid = fork();
+    if(!pid)
+    {
+        oid table_oid[] = {1, 3, 6, 1, 4, 1, 4, 199, 1};
+        Core core(3, table_oid, sizeof(table_oid));
+    
+        core.cycle();
+    }
+    
+    return pid;
+}
+
 int main()
 {
-	if(geteuid() != 0)
-	{
-	    printf("The program requires root privileges\n");
-	    exit(EXIT_FAILURE);
-	}
-	
-	oid table_oid[] = {1, 3, 6, 1, 4, 1, 4, 199, 1};
-	Core core(3, table_oid, sizeof(table_oid));
-	
-	core.cycle();
+    if(geteuid() != 0)
+    {
+        printf("The program requires root privileges\n");
+        exit(EXIT_FAILURE);
+    }
+    
+    signal(SIGTERM, signal_handler);
+    signal(SIGINT, signal_handler);
+    
+    pid_t pid = -1;
+    while(work)
+    {
+        int status = 0;
+        if(pid <= 0 || waitpid(pid, &status, WNOHANG) > 0)
+            pid = start_program();
+        
+        sleep(1);
+    }
+    
+    if(pid > 0)
+        kill(pid, 9);
 }
